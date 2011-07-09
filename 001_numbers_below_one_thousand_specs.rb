ced66bb7095d4f277774683990b99af2dafc3ebf
Dane Lipscombe <dane@lipscobme.com.au>
2011-07-09 22:33:06 +1000

    adding before each to simplify spec

--- 001_numbers_below_one_thousand_specs.rb
+++ 001_numbers_below_one_thousand_specs.rb
@@ -7,27 +7,27 @@ require './001_numbers_below_one_thousand'
 
 
 describe Processor do
+  before(:all) do 
+    @processor = Processor.new
+  end
+
   describe "#sum" do
     it "returns the correct answer" do
-      processor = Processor.new
-      processor.sum.should == 233168
+      @processor.sum.should == 233168
     end
   end
 
   describe "#multiple_of_3_or_5?" do
     it "returns false if given 0" do
-      processor = Processor.new
-      processor.multiple_of_3_or_5?(0).should == false
+      @processor.multiple_of_3_or_5?(0).should == false
     end
 
     it "returns true if given 3" do
-      processor = Processor.new
-      processor.multiple_of_3_or_5?(3).should == true
+      @processor.multiple_of_3_or_5?(3).should == true
     end
 
     it "returns true if given 5" do
-      processor = Processor.new
-      processor.multiple_of_3_or_5?(5).should == true
+      @processor.multiple_of_3_or_5?(5).should == true
     end
   end
 
